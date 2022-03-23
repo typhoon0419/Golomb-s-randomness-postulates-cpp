204e3ee6c48a945131b8aea0fbdd30faf37380f7
颱風風 <41107339+typhoon0419@users.noreply.github.com>
2022-03-23 17:37:57 +0800

    homework complete

--- main.cpp
+++ main.cpp
@@ -1,12 +1,169 @@
-#include <iostream>
+#include<stdio.h>
+#include<math.h>
 
-int main() {
-    int length;
+#define LENGTH 7
 
-    std::cout << "Please enter the length of sequence: " << std::endl;
-    std::cin >> length;
+int found_one(int array[]){
+    int count=0;
+    for (int i = 0; i < 7; ++i) {
+        if(array[i]==1){
+            count++;
+        }
+    }
+    return count;
+}
+
+void initial_a(int array[]){
+    for (int i = 0; i < LENGTH; ++i) {
+        array[i] = 0;
+    }
+}
+
+void print_array(int array[]){
+    for(int n = 7; n > 0; n--)
+        printf("%d",array[n - 1]);
+    printf("\n");
+}
+
+void found_runs_needed(int total_runs, int array[]){
+    for (int i = 0; i < LENGTH / 2; ++i) {
+        if(total_runs == 1){
+            array[i]=1;
+            break;
+        }
+        array[i]=total_runs/2;
+        total_runs = total_runs/2;
+    }
+}
+
+bool check_total_runs(int total_runs, int array[]){
+    int pre;
+    int count_of_runs = 0;
+    for (int i = 0; i < LENGTH; ++i) {
+        if(i == 0){
+            pre = array[i];
+            count_of_runs++;
+            continue;
+        }
+        if(array[i] != pre){
+            pre = array[i];
+            count_of_runs++;
+        }
+    }
+    //printf("\ncount_of_runs = %d\n", count_of_runs);
+
+    if(total_runs == count_of_runs)
+        return true;
+    else
+        return false;
+}
+
+bool found_runs(int total_runs, int array[]){
+    int pre;
+    int count = 0;
+    int length = 0;
+    int run_count = 0;
 
 
+    for (int i = 0; i < LENGTH; ++i) {
+        if (i==0)
+            pre = array[i];
+
+        if(array[i] != pre){
+            count = 0;
+            pre = array[i];
+        }
+
+        count++;
+
+        if(count == 3)
+            return true;
+
+
+    }
+    return false;
+}
+
+void shift(int array[], int target[], int shift_len){
+    for (int i = 0; i < LENGTH; ++i) {
+        target[i] = array[((i+shift_len) % LENGTH)];
+    }
+}
+
+bool function_C(int array[], int shift_array[]){
+    int answer_of_all_C = 0;
+    int answer;
+    printf("\nC(t) of shift i\n");
+    printf("   t: ");
+    for (int k = 1; k < LENGTH; ++k)
+        printf("%2d ", k);
+    printf("\nC(t): ");
+    for (int i = 1; i < LENGTH; ++i) {
+
+        answer = 0;
+        shift(array, shift_array, i);
+        for (int j = 0; j < LENGTH; ++j) {
+            if(array[j] == shift_array[j])
+                answer ++;
+            else
+                answer --;
+        }
+        printf("%d ", answer);
+        if(i == 1)
+            answer_of_all_C = answer;
+        else if (answer!=answer_of_all_C){
+            printf("Fail when shift %d\n\n", i);
+            return false;
+        }
+    }
+    printf("\n");
+    return true;
+}
+
+int main(void)
+{
+    int i=0;
+    int n;
+    int a[LENGTH];
+    int runs[LENGTH / 2];
+    int count;
+    int R1count = 0;
+    int R2count = 0;
+    int R3count = 0;
+    int shift_array[LENGTH];
+
+    int total_runs;
+    if(LENGTH % 2)
+        total_runs = (LENGTH + 1) / 2;
+    else
+        total_runs = LENGTH / 2;
+
+    found_runs_needed(total_runs,runs);
+
+    for (int j = 0; j < pow(2, LENGTH); ++j) {
+        initial_a(a);
+        i=0;
+        n = j;
+        while (n > 0) {
+            a[i] = n % 2;
+            i = i + 1;
+            n = n/2;
+        }
+        count = found_one(a);
+        if(count==(LENGTH/2) || count==((LENGTH+1)/2)){
+            if(check_total_runs(total_runs, a)&& found_runs(total_runs, a)){
+                if(function_C(a, shift_array)){
+                    printf("%3d: ", j);
+                    print_array(a);
+                    R3count++;
+                }
+                R2count++;
+            }
+            R1count++;
+        }
+    }
+
+    printf("\nAmounts of conform to R1 = %d\nAmounts of conform to R2 = %d\nAmounts of conform to R3 = %d\n", R1count, R2count, R3count);
 
     return 0;
 }
